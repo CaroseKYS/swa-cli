b8ef335178eab227bb067db8a5709fcac3f0bf2a
康永胜 <kys8823@gmail.com>
2017-10-09 14:21:57 +0800

    调整安装流程

--- script/postinstall.js
+++ script/postinstall.js
@@ -1,29 +1,29 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
-var utils = require('../lib/utils.js');
-var logger = require('../lib/logger.js');
-var self = require('../package.json');
-var npm = utils.getNpm();
-var npmGlobalDir, ifGlobalInstall = true;
+const fs     = require('fs');
+const path   = require('path');
+const async  = require('async');
+const utils  = require('../lib/utils.js');
+const logger = require('../lib/logger.js');
+const self   = require('../package.json');
+const npm    = utils.getNpm();
+const modulesNeedInstalling = [];
+const promises = [];
+
 /*应该被全局安装的模块*/
-var modulesShouldBeInstalled = ['fis3', 'fis-parser-less', 'fis3-parser-html-uri', 
-                                'fis3-parser-get-conf', 'fis3-hook-module', 
-                                'fis-parser-babel-6.x', 'pm2', 'bower', 
-                                'nodemon', 'node-inspector', 'http-server'];
-var modulesNeedInstalling = [];
-var series = [];
+const modulesShouldBeInstalled = ['fis3', 'fis-parser-less', 'fis3-parser-html-uri', 
+  'fis3-parser-get-conf', 'fis3-hook-module', 'fis-parser-babel-6.x', 'pm2', 'bower', 
+  'nodemon', 'node-inspector', 'http-server'];
+let   npmGlobalDir;
 
 logger.info(`${self.name} 模块安装成功, 将为您自动安装 ${modulesShouldBeInstalled} 等模块.`);
 
 /*加载npm*/
-series.push(function (cb) {
-  npm.load({loaded: false, 'global': true}, cb);
-});
+promises.push(new Promise((reslove, reject) => {
+  npm.load({loaded: false, 'global': true}, reslove);
+}));
 
 /*获取全局路径*/
-series.push(function (cb) {
+promises.push(new Promise((reslove, reject) => {
   npm.commands.root(function(err, data){
     if (err) {
       npmGlobalDir = npm.config.get('globalconfig');
@@ -32,21 +32,21 @@ series.push(function (cb) {
       npmGlobalDir = data;
     }
     logger.info(`npm的全局安装路径为: ${npmGlobalDir}`);
-    cb();
+    reslove();
   });
-});
+}));
 
 /*检测应当被全局安装的模块是否全局安装*/
 modulesShouldBeInstalled.forEach(function(moduleName){
-  series.push(function(cb){
-    isModuleInstalledGlobally(moduleName, cb);
-  });
+  promises.push(new Promise((reslove, reject) => {
+    isModuleInstalledGlobally(moduleName, reslove);
+  }));
 });
 
 /*安装依赖模块*/
-series.push(function(cb){
+promises.push(new Promise((reslove, reject) => {
   if (modulesNeedInstalling.length <= 0) {
-    cb();
+    reslove();
     return;
   }
   logger.info(`开始安装 ${modulesNeedInstalling} 模块.`);
@@ -54,26 +54,23 @@ series.push(function(cb){
     if (er) {
       throw er;
     }
-    cb();
+    reslove();
   });
   npm.on('log', function (message) {
     logger.info(message);
   });
-});
+}));
 
-series.push(function (cb) {
+Promise.all(promises)
+.then(() => {
   logger.info(`${self.name} 模块安装流程完成.`);
+})
+.catch( e => {
+  logger.error(`${self.name} 模块安装流程出现错误: `, e);
 });
-
-/*执行整个步骤*/
-async.series(series);
 
 function isModuleInstalledGlobally(moduleName, cb) {
   var modulePath = path.join(npmGlobalDir, `${moduleName}`);
-  if (!ifGlobalInstall) {
-    cb();
-    return;
-  }
 
   logger.info('');
   logger.info(`--------------------------------------------------`);
