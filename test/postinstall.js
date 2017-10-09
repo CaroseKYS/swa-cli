3a285e147adf13f8d84888c4c961e911c83795c4
康永胜 <kys8823@gmail.com>
2017-10-09 17:37:32 +0800

    修改测试用例

--- test/postinstall.js
+++ test/postinstall.js
@@ -4,9 +4,13 @@ const rewire = require('rewire');
 describe('/scripts/postinstall.js 测试', () => {
   var postinstall;
 
-  describe('加载测试', () => {
-    it('', () => {
+  describe('加载测试', function() {
+    it('', function(done) {
+      this.timeout(10000);
       postinstall = rewire('../scripts/postinstall.js');
+      setTimeout(() => {
+        done();
+      }, 8000);
     });
   });
 
